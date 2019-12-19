fccf2e604df04b3ea44cbd5a64a0ea25e8ec045f
amsterg <chaitanyaamster@gmail.com>
2019-12-19 14:27:55 +0530

    Errors

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,23 +4,40 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<i32, ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
-
     if cost > tokens {
         println!("You can't afford that many!");
     } else {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+    Ok(cost)
 }
+
+// fn main() {
+//     let mut tokens = 100;
+//     let pretend_user_input = "8";
+
+//     let cost = total_cost(pretend_user_input);
+//     match cost {
+//         Ok(cost) => {
+//             if cost > tokens {
+//                 println!("You can't afford that many!");
+//             } else {
+//                 tokens -= cost;
+//                 println!("You now have {} tokens.", tokens);
+//             }
+//         }
+//         Err(cost) => println!("Err{}", cost),
+//     }
+// }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
